13a8674967949d3985a1048bb88616d414a70017
Sleepy105 <lm.sousa@fe.up.pt>
2020-04-21 19:25:44 +0100

    Integrated the Database code into the main program

---
+++ src/DBInterface.cpp
@@ -0,0 +1,324 @@
+/* 
+C++ SQLITE
+*/
+#include <string>
+#include <iostream>
+#include <stdio.h>
+#include <sqlite3.h>
+#include <time.h>
+using namespace std;
+
+static int checkDB(const char* s); //Verifica se a DB j� existe
+static int createDB(const char* s);  // Cria DB
+static int createTable(const char* s); //Cria Tabelas
+static int insertDataOrder(const char* s, int Order_Number, string Type, string State, string Initial_Piece, string Final_Piece, int Total_Pieces, int Deadline, string Entry_Time);
+/*
+Insere informa��o de uma Ordem. 
+Order_Number: Numero da ordem. Se a Ordem for carga de pe�a ent�o Order_Number = -1
+Type: S� pode ser: Transformation, Dispatch, Incoming
+State: S� pode ser: Waiting, Executing, Finished
+Initial_Piece: � a pe�a inicial
+Final_Piece: � a pe�a final, se for carga ou descarga representa a mesma pe�a que a inicial
+Total_Pieces: N�mero total de pe�as em quest�o, se for carga � 1
+DeadLine: Se for carga ou descarga � -1, este valor representa segundos
+Entry_Time: Hora de entrada do pedido. Se for carga a ordem de entrada e come�o de execu��o e o state � logo executing
+Se for tipo Incoming, a seguir a esta fun��o tem que se chamar a insertDataPiece
+*/
+static int insertDataPiece(const char* s, int Order_ID, int Order_Number, string Execution_Start);
+/*
+Insere informa��o quando uma pe�a vai para a f�brica, esta pode ser descarga, transforma�ao ou carga.
+Se for carga, esta � chamada logo a seguir � inserDataOrder, logo o ID � o ultimo ID adicionado com tipo Incoming
+*/
+static int updateDataPiece(const char* s, int Piece_ID, string Execution_END);
+/*
+Insere a hora em que a pe�a saiu da f�brica, ou porque entrou no armaz�m, ou porque foi descarregada, � necess�rio saber o ID da pe�a em quest�o
+*/
+static int getOrder_ID(const char* s, string type, int Order_Number);
+/*
+Procura o Order ID com um determinado numero de ordem, se for do tipo incoming retorna o ultimo id
+*/
+static int callback_id(void* id, int argc, char** argv, char** azColName);
+/*
+Fun�ao necessaria para fazer interface com DB quando se pede um valor da DB
+*/
+static int callback(void* Notused, int argc, char** argv, char** azColName);
+static int updateData(const char* s, string State, int Order_ID, string Time);
+/*
+Faz update da informa�ao: Se for incoming so pode ser para finished e escreve no tempo de fim 
+Se o state for executing escreve no tempo de start executing
+Se o state for finished escreve no tempo de end time.
+*/
+static int deleteData(const char* s);
+string getDateTime();
+/*
+Vai buscar a hora e a data atual e retorna no formato DD-MM-YYY HH:MM:SS
+*/
+string getDateTime()
+{
+	string Date;
+	time_t current_time;
+	struct tm  local_time;
+
+	time(&current_time);
+	//localtime_s(&local_time, &current_time);
+	localtime_r(&current_time, &local_time);
+
+	int Year = local_time.tm_year + 1900;
+	int Month = local_time.tm_mon + 1;
+	int Day = local_time.tm_mday;
+
+	int Hour = local_time.tm_hour;
+	int Min = local_time.tm_min;
+	int Sec = local_time.tm_sec;
+	Date = to_string(Day) + "-" + to_string(Month) + "-" + to_string(Year) + " " + to_string(Hour) + ":" + to_string(Min) + ":" + to_string(Sec);
+	return Date;
+}
+static int checkDB(const char* s) 
+{
+	sqlite3* DB;
+	int exit = 0;
+
+	exit = sqlite3_open_v2(s, &DB, SQLITE_OPEN_READONLY, NULL); 
+	if (exit) {
+		std::cerr << "Does not exist " << sqlite3_errmsg(DB) << std::endl;
+		return (-1);
+	}
+	else
+		std::cout << "Database exists!" << std::endl;
+	sqlite3_close(DB);
+
+	return 0;
+}
+static int createDB(const char* s)
+{
+	sqlite3* DB;
+	int exit = 0;
+	exit = sqlite3_open(s, &DB);
+	if (exit) {
+		std::cerr << "Error open DB " << sqlite3_errmsg(DB) << std::endl;
+		return (-1);
+	}
+	else
+		std::cout << "Opened Database Successfully!" << std::endl;
+	sqlite3_close(DB);
+
+	return 0;
+}
+static int createTable(const char* s)
+{
+	sqlite3* DB;
+	string sql = ("CREATE TABLE IF NOT EXISTS ORDERS(" \
+		"ID INTEGER PRIMARY KEY AUTOINCREMENT, " \
+		"Order_Number INT, " \
+		"Type TEXT NOT NULL CHECK(Type ='Transformation' OR Type ='Dispatch' OR Type = 'Incoming'), " \
+		"State TEXT NOT NULL DEFAULT 'Waiting' CHECK( State='Waiting' OR State ='Executing' OR State = 'Finished'), " \
+		"Initial_Piece CHAR(3) NOT NULL, " \
+		"Final_Piece CHAR(3) NOT NULL, " \
+		"Total_Number_Pieces INT NOT NULL, " \
+		"Deadline TEXT, " \
+		"Entry_Time TEXT NOT NULL, " \
+		"End_Time TEXT, "\
+		"Execution_Start TEXT, "\
+		"Remaining_Time TEXT);" \
+		"CREATE TABLE IF NOT EXISTS Piece(" \
+		"ID INTEGER PRIMARY KEY AUTOINCREMENT, "\
+		"ID_ORDER INT REFERENCES ORDERS(ID) ON DELETE CASCADE ON UPDATE CASCADE, "\
+		"Order_Number INT REFERENCES ORDERS(Order_Number) ON DELETE CASCADE ON UPDATE CASCADE, " \
+		"Execution_Start TEXT NOT NULL, " \
+		"Execution_End TEXT);");
+	try
+	{
+		int exit = 0;
+		exit = sqlite3_open(s, &DB);
+
+		char* messageError;
+		exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messageError);
+		if (exit != SQLITE_OK)
+		{
+			cerr << "Error Create Table" << endl;
+			sqlite3_free(messageError);
+		}
+		else
+			cout << "Table created Successfully" << endl;
+		sqlite3_close(DB);
+	}
+	catch (const exception& e)
+	{
+		cerr << e.what();
+	}
+	return 0;
+}
+static int insertDataOrder(const char* s, int Order_Number, string Type, string State,  string Initial_Piece, string Final_Piece, int Total_Pieces, int Deadline, string Entry_Time)
+{
+	sqlite3* DB;
+	char* messageError;
+	int exit = sqlite3_open(s, &DB);
+	string sql;
+	if (Type == "Incoming")
+	{
+		sql = ("INSERT INTO ORDERS (Order_Number, Type, State, Initial_Piece, Final_Piece, Total_Number_Pieces, Deadline, Entry_Time, Execution_Start)" \
+			" VALUES(" \
+			+ to_string(Order_Number) + " ," \
+			+ "'" + Type + "'" + " ," \
+			+ "'" + "Executing" + "'" + " ," \
+			+ "'" + Initial_Piece + "'" + " ," \
+			+ "'" + Final_Piece + "'" + " ," \
+			+ to_string(Total_Pieces) + " ," \
+			+ "'" + to_string(Deadline) + "'" + " ," \
+			+ "'" + Entry_Time + "' ," \
+			+ "'" + Entry_Time + "'" \
+			+ ");");
+	}
+	else
+	{
+		sql = ("INSERT INTO ORDERS (Order_Number, Type, State, Initial_Piece, Final_Piece, Total_Number_Pieces, Deadline, Entry_Time)" \
+			" VALUES(" \
+			+ to_string(Order_Number) + " ," \
+			+ "'" + Type + "'" + " ," \
+			+ "'" + State + "'" + " ," \
+			+ "'" + Initial_Piece + "'" + " ," \
+			+ "'" + Final_Piece + "'" + " ," \
+			+ to_string(Total_Pieces) + " ," \
+			+ "'" + to_string(Deadline) + "'" + " ," \
+			+ "'" + Entry_Time + "'" \
+			+ ");");
+	}
+	exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messageError);
+	if (exit != SQLITE_OK)
+	{
+		cerr << "Error Insert" << endl;
+		sqlite3_free(messageError);
+	}
+	else
+		cout << "Records created Successfully" << endl; 
+	sqlite3_close(DB);
+	return 0;
+}
+static int insertDataPiece(const char* s, int Order_ID, int Order_Number, string Execution_Start)
+{
+	sqlite3* DB;
+	char* messageError;
+	int exit = sqlite3_open(s, &DB);
+	string sql = ("INSERT INTO Piece(ID_ORDER, Order_Number, Execution_Start) VALUES(" \
+		+ to_string(Order_ID) + " ," \
+		+ to_string(Order_Number) + " ," \
+		+ "'" + Execution_Start + "'" \
+		+ ");");
+	exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messageError);
+	if (exit != SQLITE_OK)
+	{
+		cerr << "Error Insert" << endl;
+		sqlite3_free(messageError);
+	}
+	else
+		cout << "Records created Successfully" << endl;
+	sqlite3_close(DB);
+	return 0;
+}
+static int updateDataPiece(const char* s, int Piece_ID, string Execution_END)
+{
+	sqlite3* DB;
+	char* messageError;
+	int exit = sqlite3_open(s, &DB);
+	string sql = ("UPDATE Piece SET Execution_END = '" + Execution_END + "' WHERE ID = " + to_string(Piece_ID));
+
+	exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messageError);
+	if (exit != SQLITE_OK)
+	{
+		cerr << "Error Insert" << endl;
+		sqlite3_free(messageError);
+	}
+	else
+		cout << "Records created Successfully" << endl;
+	sqlite3_close(DB);
+	return 0;
+}
+static int getOrder_ID(const char* s, string type, int Order_Number)
+{
+	sqlite3* DB;
+	int exit = sqlite3_open(s, &DB);
+	int id = 0;
+	string sql;
+	if (type == "Transformation" || type == "Dispatch")
+	{
+		sql = "SELECT ID FROM ORDERS WHERE Order_Number = " \
+			+ to_string(Order_Number) + " AND Type = '" + type + "';";
+	}
+	else if (type == "Incoming")
+	{
+		sql = "SELECT ID FROM ORDERS WHERE Type = 'Incoming' ORDER BY ID DESC LIMIT 1;";
+	}
+	/* An open database, SQL to be evaluated, callback function, 1st argument to callback, error msg written here*/
+	exit = sqlite3_exec(DB, sql.c_str(), callback_id, &id, NULL);
+	if (exit != SQLITE_OK) {
+		std::cerr << "Error in select statement " << endl;
+	}
+	else {
+		std::cerr << "Records returned" << endl;
+	}
+	return id;
+}
+static int callback_id(void* id, int argc, char** argv, char** azColName)
+{
+	int *c = static_cast<int *>(id);
+	*c = atoi(argv[0]);
+	return 0;
+}
+// note that callback is called each time with the information of row
+// if we have 6 rows, callback it will be called 6 times
+// retrieve contens of database used by selectData()
+/* argc: holds the number of results, azColName: holds each column returned in array, argv: holds each value in array*/
+static int callback(void* NotUsed, int argc, char** argv, char** azColName)
+{
+	for (int i = 0; i < argc; i++) {
+		//column name and value
+		cout << azColName[i] << ": " << argv[i] << endl;
+	}
+	cout << endl;
+	return 0;
+}
+static int updateData(const char* s, string State, int Order_ID, string Time)
+{
+	sqlite3* DB;
+	char* messageError;
+	int exit = sqlite3_open(s, &DB);
+	string sql1;
+	string sql = ("UPDATE ORDERS SET State = '" + State + "' WHERE ID = " + to_string(Order_ID));
+
+	exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messageError);
+	if (exit != SQLITE_OK)
+	{
+		cerr << "Error Insert" << endl;
+		sqlite3_free(messageError);
+	}
+	else
+		cout << "Records created Successfully" << endl;
+	if (State == "Finished")
+	{
+		sql1 = ("UPDATE ORDERS SET End_Time = '" + Time + "' WHERE ID = " + to_string(Order_ID));
+	}
+	else if (State == "Executing")
+	{
+		sql1 = ("UPDATE ORDERS SET Execution_Start = '" + Time + "' WHERE ID = " + to_string(Order_ID));
+	}
+	exit = sqlite3_exec(DB, sql1.c_str(), NULL, 0, &messageError);
+	if (exit != SQLITE_OK)
+	{
+		cerr << "Error Insert" << endl;
+		sqlite3_free(messageError);
+	}
+	else
+		cout << "Records created Successfully" << endl;
+	sqlite3_close(DB);
+	return 0;
+}
+static int deleteData(const char* s)
+{
+	sqlite3* DB;
+	int exit = sqlite3_open(s, &DB);
+	string sql1 = "DELETE FROM Piece;";
+	sqlite3_exec(DB, sql1.c_str(), callback, NULL, NULL);
+	string sql = "DELETE FROM ORDERS;";
+	sqlite3_exec(DB, sql.c_str(), callback, NULL, NULL);
+	return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 
 #include "UDPManager.cpp"
 #include "XMLParser.cpp"
+//#include "OPC-UA.cpp"
+#include "DBInterface.cpp"
+
+#include "helpers.h"
 
 int main (int argc, char const *argv[]) {
 
@@ -11,7 +15,82 @@ int main (int argc, char const *argv[]) {
     UDPManager UDPManager(54321);
     std::thread udp_worker = UDPManager.spawn_worker(XMLParser.parseString);
 
-    udp_worker.join();
+    FILE* f = fopen("opc-ua-id.txt", "r");
+    if (!f) {
+        log(ERROR) << "Couldn't open opc-ua-id.txt" << std::endl;
+    }
+    char OpcUa_id[100] = {0};
+    int result = fread (OpcUa_id,1,100,f);
     
+    /*OPCUA_Manager myManager("opc.tcp://127.0.0.1:4840", OpcUa_id, 4);
+
+    if (myManager.Is_Connected()) {
+
+        piece_object peca;
+        peca.id_piece = 1;
+        peca.transformation = 4;
+        peca.type_piece = 6;
+        for (uint16_t i = 0; i < 59; i++) {
+            peca.path[i] = 0;
+        }
+        peca.path[0] = RIGHT;
+        peca.path[1] = RIGHT;
+        peca.path[2] = DOWN;
+        peca.path[3] = DOWN;
+        peca.path[4] = RIGHT;
+        peca.path[5] = LEFT;
+        peca.path[6] = DOWN;
+        peca.path[7] = DOWN;
+        peca.path[8] = DOWN;
+        peca.path[9] = DOWN;
+        peca.path[10] = LEFT;
+        peca.path[11] = LEFT;
+
+        myManager.SendPieceOPC_UA(peca.path, peca.transformation, peca.id_piece, peca.type_piece, 1);
+        std::cout << "Press ENTER to send next piece..." << std::endl;
+        std::string aux;
+
+        peca.path[0] = RIGHT;
+        peca.path[1] = RIGHT;
+        peca.path[2] = DOWN;
+        peca.path[3] = DOWN;
+        peca.path[4] = LEFT;
+        peca.path[5] = RIGHT;
+        peca.path[6] = DOWN;
+        peca.path[7] = DOWN;
+        peca.path[8] = DOWN;
+        peca.path[9] = DOWN;
+        peca.path[10] = LEFT;
+        peca.path[11] = LEFT;
+        std::getline(std::cin, aux);
+        myManager.SendPieceOPC_UA(peca.path, peca.transformation, peca.id_piece, peca.type_piece, 1);
+    }
+    else {
+        std::cout << "!!!Failed to Connect to OPC-UA Master!!!" << std::endl;
+    }*/
+
+    const char* dir = "C:\\Users\\andre\\Desktop\\sqltesteLIBGEN\\factory.db"; // Definir path da DB
+	checkDB(dir);
+	createDB(dir);
+	createTable(dir);
+	deleteData(dir);
+	int num_Order = 4;
+	string Type = "Transformation";
+	string State = "Waiting";
+	char Initial_Piece[3] = "P1";
+	char Final_Piece[3] = "P5";
+	int Total_Pieces = 40;
+	int Deadline = 400;
+	string NewState = "Executing";
+	insertDataOrder(dir, num_Order, Type, State, Initial_Piece, Final_Piece, Total_Pieces, Deadline, getDateTime());
+	insertDataPiece(dir, getOrder_ID(dir, Type, 4), 4, getDateTime());
+	updateData(dir, NewState, getOrder_ID(dir, Type, num_Order), getDateTime());
+
+
+
+
+
+    // Wait for threads to close
+    udp_worker.join();
     return 0;
 }
