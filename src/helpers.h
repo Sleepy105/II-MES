bf69c4f2dc2618b7bb86c64f4f9544155e790b4b
Sleepy105 <lm.sousa@fe.up.pt>
2020-04-22 00:11:09 +0100

    Colourize meslog outputs

--- src/helpers.h
+++ src/helpers.h
@@ -3,16 +3,24 @@
 
 /* Compilers must hate me :) */
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+/* Add more options here */
 #define INFO "info"
 #define ERROR "error"
-#define WARNING "warning"
+#define WARNING "warn"
+
+#define log_INFO std::cout << ANSI_COLOR_GREEN << INFO << ANSI_COLOR_RESET
+#define log_ERROR std::cerr << ANSI_COLOR_RED << ERROR << ANSI_COLOR_RESET
+#define log_WARNING std::cout << ANSI_COLOR_YELLOW << WARNING << ANSI_COLOR_RESET
 
 #define meslog(type) log_##type meta_log
-
-#define log_INFO std::cout << INFO 
-#define log_ERROR std::cerr << ERROR 
-#define log_WARNING std::cout << WARNING 
-
 #define meta_log << ":" << __FILE__ << ":" << __func__ << ":" << __LINE__ << " \t"
 
 #endif
