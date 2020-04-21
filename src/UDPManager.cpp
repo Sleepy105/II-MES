424482f9b66454e514f02e8bcf12519d80917e3b
Sleepy105 <lm.sousa@fe.up.pt>
2020-04-21 23:13:11 +0100

    Split UDPManager into source and header files

--- src/UDPManager.cpp
+++ src/UDPManager.cpp
@@ -8,57 +8,7 @@
  * 
  */
 
-#include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
-#include <thread>
-
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <sys/socket.h>
-
-#include "helpers.h"
-
-#define UDPMANAGER_DEFAULT_BUFFER_SIZE 2000
-
-class UDPManager {
-private:
-    uint16_t port;
-    uint32_t buffer_size = UDPMANAGER_DEFAULT_BUFFER_SIZE; // Has to be larger than the max length of an UDP/IP Packet
-    int socket_fd = -1;
-    sockaddr_in server, client;
-    socklen_t serverlen, clientlen;
-    char* buffer = NULL;
-
-    void _worker(void (*callback)(std::string));
-public:
-    /**
-     * @brief Construct a new UDPManager object
-     * 
-     * @param port UPD Port to listen for cummunications
-     */
-    UDPManager(uint16_t port);
-
-    /**
-     * @brief Construct a new UDPManager object
-     * 
-     * @param port UPD Port to listen for cummunications
-     * @param buffer_size Size of the data buffer
-     */
-    UDPManager(uint16_t port, uint32_t buffer_size);
-
-    /**
-     * @brief Destroy the UDPManager object
-     * 
-     */
-    ~UDPManager();
-
-    /**
-     * @brief Spawn a new thread, which listens to the selected port
-     * 
-     */
-    std::thread spawn_worker(void (*callback)(std::string));
-};
+#include "UDPManager.hpp"
 
 UDPManager::UDPManager(uint16_t port, uint32_t buffer_size) : port(port), buffer_size(buffer_size) {
     socket_fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
