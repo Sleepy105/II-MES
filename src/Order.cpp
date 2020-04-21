8bcebd71ba17de64c282e0926634598ecfb2a1c9
Sleepy105 <lm.sousa@fe.up.pt>
2020-04-21 23:12:59 +0100

    Split Order into source and header files

--- src/Order.cpp
+++ src/Order.cpp
@@ -8,33 +8,8 @@
  * 
  */
 
-#include <stdlib.h>
-#include <iostream>
-#include <chrono>
-
-#include "helpers.h"
-
-namespace Order {
-    class BaseOrder;
-    uint8_t ORDER_TYPE_TRANSFORMATON = 0;
-    uint8_t ORDER_TYPE_UNLOAD = 1;
-    uint8_t ORDER_TYPE_REQUESTSTORES = 2;
-};
-
+#include "Order.hpp"
 using namespace Order;
-class Order::BaseOrder {
-private:
-    uint8_t order_id = 0;
-    uint8_t order_type = 0;
-    uint32_t quantity = 0;
-    uint64_t creation_epoch = 0;
-    uint64_t deadline = 0;
-public:
-    BaseOrder(uint8_t order_id, uint8_t order_type);
-    ~BaseOrder();
-
-    bool is_valid();
-};
 
 BaseOrder::BaseOrder(uint8_t order_id, uint8_t order_type) : order_id(order_id), order_type(order_type) {
     log(INFO) << "ORDER " << std::to_string(order_id) << " created." << std::endl;
---
+++ src/Order.hpp
@@ -0,0 +1,41 @@
+/**
+ * @file Order.hpp
+ * @brief 
+ * @version 0.1
+ * @date 2020-04-21
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#ifndef _ORDER_HPP_
+#define _ORDER_HPP_
+
+#include <stdlib.h>
+#include <iostream>
+#include <chrono>
+
+#include "helpers.h"
+
+namespace Order {
+    class BaseOrder;
+    const uint8_t ORDER_TYPE_TRANSFORMATON = 0;
+    const uint8_t ORDER_TYPE_UNLOAD = 1;
+    const uint8_t ORDER_TYPE_REQUESTSTORES = 2;
+};
+
+class Order::BaseOrder {
+private:
+    uint8_t order_id = 0;
+    uint8_t order_type = 0;
+    uint32_t quantity = 0;
+    uint64_t creation_epoch = 0;
+    uint64_t deadline = 0;
+public:
+    BaseOrder(uint8_t order_id, uint8_t order_type);
+    ~BaseOrder();
+
+    bool is_valid();
+};
+
+#endif
